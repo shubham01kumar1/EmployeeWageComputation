744161d4165409a71824471da3a1a42aa0357c5e
shubham01kumar1 <shubham01kumar1@gmail.com>
2021-08-11 21:42:41 +0530

    Adding employee part time feature

--- EmpWage.java
+++ EmpWage.java
@@ -1,17 +1,19 @@
 public class EmpWage {
     public static void main(String[] args) {
-        int IS_PRESENT=1;
+        int IS_PRESENT_FULL_TIME=1;
+        int IS_PRESENT_HALF_TIME=2;
         int WAGE_PER_HOUR=20;
+        int empWorkHrs=0;
         int empWage=0;
-        double empCheck=Math.floor(Math.random()*10)%2; //to randomly generate num. 0 or 1
+        double empCheck=Math.floor(Math.random()*10)%3; //to randomly generate num. 0 or 1
 
-
-        if(empCheck==IS_PRESENT) {
-            int empHrs = 8;
-            empWage = WAGE_PER_HOUR * empHrs;
-        }
+        if(empCheck==IS_PRESENT_FULL_TIME)
+            empWorkHrs=8;
+        else if(empCheck==IS_PRESENT_HALF_TIME)
+            empWorkHrs=4;
         else
-            empWage=0;
-        System.out.println("Employee daily wage is :"+empWage);
+            empWorkHrs=0;
+        empWage=WAGE_PER_HOUR * empWorkHrs;
+        System.out.println("Employee's day wage is :"+empWage);
     }
 }
