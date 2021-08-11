1048d849b1ed3a8a35d439904460cac2fe95ee01
shubham01kumar1 <shubham01kumar1@gmail.com>
2021-08-11 23:21:39 +0530

    Adding Employee monthly wage

--- EmpWage.java
+++ EmpWage.java
@@ -3,22 +3,25 @@ public class EmpWage {
         final int IS_PRESENT_FULL_TIME=1;
         final int IS_PRESENT_HALF_TIME=2;
         final int WAGE_PER_HOUR=20;
-        int empWorkHrs=0;
-        int empWage=0;
-        int empCheck=(int)Math.floor(Math.random()*10)%3; //to randomly generate num. 0 or 1
+        final int MAX_WORK_DAYS=20;
+        int totalSalary=0;
 
-        switch (empCheck) {
-            case IS_PRESENT_FULL_TIME:
-                empWorkHrs = 8;
-                break;
-            case IS_PRESENT_HALF_TIME:
-                empWorkHrs = 4;
-                break;
-            default:
-                empWorkHrs = 0;
-                break;
+        for(int i=1;i<=MAX_WORK_DAYS;i++) {
+            int empWorkHrs=0;
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3; //to randomly generate num. 0 or 1
+            switch (empCheck) {
+                case IS_PRESENT_FULL_TIME:
+                    empWorkHrs = 8;
+                    break;
+                case IS_PRESENT_HALF_TIME:
+                    empWorkHrs = 4;
+                    break;
+                default:
+                    empWorkHrs = 0;
+                    break;
+            }
+            totalSalary+= WAGE_PER_HOUR * empWorkHrs;
         }
-        empWage=WAGE_PER_HOUR * empWorkHrs;
-        System.out.println("Employee's day wage is :"+empWage);
+        System.out.println("Employee Monthly Wage is:"+totalSalary);
     }
 }
