2406e4bad9ffd6bf2ee8983c3adac54a7b94c130
shubham01kumar1 <shubham01kumar1@gmail.com>
2021-08-11 19:39:27 +0530

    Employee Attendance feature

--- EmpWage.java
+++ EmpWage.java
@@ -1,5 +1,11 @@
 public class EmpWage {
     public static void main(String[] args) {
-        System.out.println("Welcome to employee wage problem");
+        int PRESENT=1;
+        int ABSENT=0;
+        double empCheck=Math.floor(Math.random()*10)%2;
+        if(empCheck==PRESENT)
+            System.out.println("Employee is Present");
+        else
+            System.out.println("Employee is Absent");
     }
 }
