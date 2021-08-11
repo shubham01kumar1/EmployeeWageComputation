83faadb6edce8a802fb535de6f13d1f499d1230d
shubham01kumar1 <shubham01kumar1@gmail.com>
2021-08-11 22:56:28 +0530

    Adding case statement feature

--- EmpWage.java
+++ EmpWage.java
@@ -1,18 +1,23 @@
 public class EmpWage {
     public static void main(String[] args) {
-        int IS_PRESENT_FULL_TIME=1;
-        int IS_PRESENT_HALF_TIME=2;
-        int WAGE_PER_HOUR=20;
+        final int IS_PRESENT_FULL_TIME=1;
+        final int IS_PRESENT_HALF_TIME=2;
+        final int WAGE_PER_HOUR=20;
         int empWorkHrs=0;
         int empWage=0;
-        double empCheck=Math.floor(Math.random()*10)%3; //to randomly generate num. 0 or 1
+        int empCheck=(int)Math.floor(Math.random()*10)%3; //to randomly generate num. 0 or 1
 
-        if(empCheck==IS_PRESENT_FULL_TIME)
-            empWorkHrs=8;
-        else if(empCheck==IS_PRESENT_HALF_TIME)
-            empWorkHrs=4;
-        else
-            empWorkHrs=0;
+        switch (empCheck) {
+            case IS_PRESENT_FULL_TIME:
+                empWorkHrs = 8;
+                break;
+            case IS_PRESENT_HALF_TIME:
+                empWorkHrs = 4;
+                break;
+            default:
+                empWorkHrs = 0;
+                break;
+        }
         empWage=WAGE_PER_HOUR * empWorkHrs;
         System.out.println("Employee's day wage is :"+empWage);
     }
