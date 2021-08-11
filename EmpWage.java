528e570e41e94ea0fcb6221242becd56f2b542ea
shubham01kumar1 <shubham01kumar1@gmail.com>
2021-08-11 20:54:00 +0530

    Adding calculation of employee daily wage feature

--- EmpWage.java
+++ EmpWage.java
@@ -1,11 +1,17 @@
 public class EmpWage {
     public static void main(String[] args) {
-        int PRESENT=1;
-        int ABSENT=0;
-        double empCheck=Math.floor(Math.random()*10)%2;
-        if(empCheck==PRESENT)
-            System.out.println("Employee is Present");
+        int IS_PRESENT=1;
+        int WAGE_PER_HOUR=20;
+        int empWage=0;
+        double empCheck=Math.floor(Math.random()*10)%2; //to randomly generate num. 0 or 1
+
+
+        if(empCheck==IS_PRESENT) {
+            int empHrs = 8;
+            empWage = WAGE_PER_HOUR * empHrs;
+        }
         else
-            System.out.println("Employee is Absent");
+            empWage=0;
+        System.out.println("Employee daily wage is :"+empWage);
     }
 }
